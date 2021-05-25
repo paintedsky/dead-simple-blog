ea4fc67f42c61cbdee0a85aef5e287254f9c68a1
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-05-26 00:45:34 +0100

    Update amp.php

--- amp.php
+++ amp.php
@@ -1,14 +1,12 @@
 <?php
-
 require 'ampify.php';
-
 ?>
 <!DOCTYPE html>
 <html ⚡ lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href​="https://fonts.googleapis.com/css?family=Trirong"​>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
     <link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js">
     <script async src="https://cdn.ampproject.org/v0.js"></script>
     <!-- Import other AMP Extensions here -->
@@ -16,39 +14,39 @@ require 'ampify.php';
     body {
 	font-family: "Tritong", serif;
 	line-height: 1.5;
-}
-body>* {
+    }
+    body>* {
 	max-width: 40em;
 	margin: 0 auto;
-}
-a, a:link, a:visited {
+    }
+    a, a:link, a:visited {
 	color: #08c;
-}
-a:hover {
+    }
+    a:hover {
 	color: #0bf;
-}
-a:active {
+    }
+    a:active {
 	color: #046;
-}
-h1.blog-title a {
+    }
+    h1.blog-title a {
 	text-decoration: none;
-}
-h2.list-title a {
+    }
+    h2.list-title a {
 	color: #000;
 	text-decoration: none;
-}
-hr {
+    }
+    hr {
 	border: none;
 	border-top: 1px solid #ccc;
-}
-footer {
+    }
+    footer {
 	color: #888;
 	padding: 1em;
 	margin: 2em auto 0;
 	background: #eee;
 	font-style: italic;
 	box-sizing: border-box;
-}
+    }
     </style>
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 
@@ -65,7 +63,7 @@ footer {
         </article>
 	<footer>
 		This blog does not offer comment functionality. If you'd like to discuss any of the topics 
-		written about here, you can <a href="mailto:blog@paintedsky.ca">send an email</a>.
+		written about here, you can <a href="mailto:<?php echo $contact_email; ?>">send an email</a>.
 	</footer>
 </body>
 </html>
